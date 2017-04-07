575e7a715ae4848263c0f7d98cad70eefd4dece8
Robien <Robien@users.noreply.github.com>
2017-04-07 11:21:50 +0200

    file must send hash at the begining of their name

--- index.js
+++ index.js
@@ -1,46 +1,87 @@
 #!/usr/bin/env node
 
-var args = process.argv.slice(2);
-var host = process.env.HOST || '0.0.0.0';
-var http_port = process.env.HTTP_PORT || 8090;
-var folder = process.env.FOLDER || process.cwd() + '/uploads';
-var pem_folder = process.env.PEM_FOLDER || process.cwd() + '/';
+var args = process.argv.slice(2)
+var host = process.env.HOST || '0.0.0.0'
+var http_port = process.env.HTTP_PORT || 8090
+var folder = process.env.FOLDER || process.cwd() + '/uploads'
+var pem_folder = process.env.PEM_FOLDER || process.cwd() + '/'
 
-var fs = require('fs');
-var http = require('http');
-var express = require('express');
-var multer = require('multer');
-var app = express();
+var fs = require('fs')
+var http = require('http')
+var express = require('express')
+var multer = require('multer')
+var app = express()
 
-fs.existsSync(folder) || fs.mkdirSync(folder);
+if (!fs.existsSync(folder))
+{
+    fs.mkdirSync(folder)
+}
 
-var storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, folder);
-  },
-  filename: function(req, file, cb) {
-    var fieldName = 'file';
-    req.body[fieldName] ? cb(null, req.body[fieldName]) : cb(null, file.originalname);
-  }
+var storage = multer.diskStorage(
+{
+    destination: function(req, file, cb)
+    {
+        var fieldName = 'file'
+        var fileName = req.body[fieldName] ? req.body[fieldName] : file.originalname
+        var hash = fileName.split('-')[0]
+        if (hash && hash.length === 40)
+        {
+            var path = folder + '/' + hash
+		    if (fs.existsSync(path))
+		    {
+			    cb('File already uploaded!')
+		    }
+		    else
+		    {
+            fs.mkdirSync(path)
+            cb(null, path)
+		    }
+        }
+        else
+        {
+		cb('Wrong file format')
+        }
+    },
+    filename: function(req, file, cb)
+    {
+        var fieldName = 'file'
+        var fileName = req.body[fieldName] ? req.body[fieldName] : file.originalname
+        var finalFileName = fileName.split('-')[1]
+        if (finalFileName)
+        {
+            cb(null, finalFileName)
+        }
+        else
+        {
+		cb('Wrong file format')
+        }
+    }
+})
+
+var upload = multer(
+{
+    storage: storage
+})
+
+app.use(function(req, res, next)
+{
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
 });
 
-var upload = multer({ storage: storage });
+app.get('/', function(req, res)
+{
+    res.send('<form action="/" method="POST" enctype="multipart/form-data">\n  <input type="file" name="file">\n  <input type="submit" value="Upload File">\n</form>\n')
+})
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.post('/', upload.any(), function(req, res)
+{
+    console.log('[' + new Date() + '] - File uploaded:', req.files[0].path)
+    res.end()
+})
 
-app.get('/', function(req, res) {
-  res.send('<form action="/media" method="POST" enctype="multipart/form-data">\n  <input type="file" name="file">\n  <input type="submit" value="Upload File">\n</form>\n');
-});
-
-app.post('/', upload.any(), function(req, res) {
-  console.log('[' + new Date() + '] - File uploaded:', req.files[0].path);
-  res.end();
-});
-
-http.createServer(app).listen(http_port, host, function() {
-  console.log('[' + new Date() + '] - HTTP File Upload Server started on ' + host + ':' + http_port);
-});
+http.createServer(app).listen(http_port, host, function()
+{
+    console.log('[' + new Date() + '] - HTTP File Upload Server started on ' + host + ':' + http_port)
+})
